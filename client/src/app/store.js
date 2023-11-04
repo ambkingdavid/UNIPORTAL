fdaf91df0628ba5a3b23a2fd621545903204bccb
GeneralBlaze <benjaminprincewill@gmail.com>
2023-11-04 12:43:34 +0100

    added persist gate to maintain state

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,39 +1,35 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import userReducer from "../slices/userSlice";
-// import signupReducer from "../slices/signupSlice";
-// import storage from 'redux-persist/lib/storage';
-// import { persistReducer } from 'redux-persist';
-// import { combineReducers } from '@reduxjs/toolkit';
-
-// const persistConfig = {
-//     key: 'root',
-//     version: 1,
-//     storage,
-//     whitelist: ['user', 'signup'],
-//     //blacklist: ['user', 'signup'],
-// };
-
-// const rootReducer = combineReducers({
-//     user: userReducer,
-//     signup: signupReducer,
-// });
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-
-
-// export default configureStore({
-//     reducer: persistedReducer
-//     });
-
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/userSlice";
 import signupReducer from "../slices/signupSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
 
-export default configureStore({
-    reducer: {
-        user: userReducer,
-        signup: signupReducer,
-    },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["user", "signup"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  signup: signupReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default configureStore({
+  reducer: persistedReducer,
+});
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from "../slices/userSlice";
+// import signupReducer from "../slices/signupSlice";
+
+// export default configureStore({
+//     reducer: {
+//         user: userReducer,
+//         signup: signupReducer,
+//     },
+// });
