12cb571a0a501ccd474734478f9871109e8a3df5
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-11-09 20:19:41 +0100

    Optional chaining

--- client/src/components/body/userData.jsx
+++ client/src/components/body/userData.jsx
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux";
 
 export const UserData = () => {
   const userData = useSelector((state) => state.user);
-  const profileFirstname = userData.profile.firstName;
-  const profileLastname = userData.profile.lastName;
+  const profileFirstname = userData.profile?.firstName;
+  const profileLastname = userData.profile?.lastName;
 
-  const dep = userData.program.name;
-  const semester = userData.user.semester;
-  const level = userData.user.level;
+  const dep = userData.program?.name;
+  const semester = userData.user?.semester;
+  const level = userData.user?.level;
 
-  const filteredCourses = userData.program.info.filter((info) => {
+  const filteredCourses = userData.program?.info?.filter((info) => {
     // Check if the semester and level match
     return info.semester === semester && Math.floor(level / 100) === info.year;
   });
@@ -17,15 +17,15 @@ export const UserData = () => {
   //array of objects that match the specified semester and level.
 
   // To get the courses from the first matched item (if any), you can do:
-  const courses = filteredCourses.length > 0 ? filteredCourses[0].courses : [];
+  const courses = filteredCourses?.length > 0 ? filteredCourses[0].courses : [];
 
   const fullName = profileFirstname + " " + profileLastname;
   const department = dep;
 
-  const programId = userData.user.ProgramId;
-  const studentId = userData.user.id;
-  const courseOfStudy = userData.user.courseOfStudy;
-  const degree = userData.user.degree;
+  const programId = userData.user?.ProgramId;
+  const studentId = userData.user?.id;
+  const courseOfStudy = userData.user?.courseOfStudy;
+  const degree = userData.user?.degree;
 
   return {
     fullName,
